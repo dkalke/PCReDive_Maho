98f84c81238584e9057b83a123960097c023c21b
宏諭 柯 <tt960945@gmail.com>
2021-08-02 02:43:37 +0800

    修正 當新增戰隊時，提前週目數沒有被正確的設定的問題

--- PCReDive_Maho/Event/Command.py
+++ PCReDive_Maho/Event/Command.py
@@ -97,8 +97,8 @@ async def on_message(message):
                   # 查無該戰隊資料，新增一筆，預設1週目1王，除當前週目外，可往後預約4週目
                   if not row: 
                     cursor = connection.cursor(prepared=True)
-                    sql = "INSERT INTO princess_connect.group (server_id, group_serial, now_boss, now_week, week_offset, boss_change) VALUES (?, ?, ?, ?, ?, ?)"
-                    data = (message.guild.id, group_serial, 1, 1, 4, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) #
+                    sql = "INSERT INTO princess_connect.group (server_id, group_serial, now_boss, now_week, week_offset, week_offset_1, week_offset_2, week_offset_3, week_offset_4, week_offset_5, boss_change) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+                    data = (ctx.guild.id, group_serial, 1, 1, 4, 4, 4, 4, 4, 4, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) #
                     cursor.execute(sql, data)
                     cursor.close
 
