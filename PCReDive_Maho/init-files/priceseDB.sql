147ea5b3ae40b3bf2ab176f1d4872b9d8f07d234
宏諭 柯 <tt960945@gmail.com>
2021-08-04 15:54:38 +0800

    環境變數設定檔與建立資料庫script

---
+++ PCReDive_Maho/init-files/priceseDB.sql
@@ -0,0 +1,133 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- 傾印 princess_connect 的資料庫結構
+CREATE DATABASE IF NOT EXISTS `princess_connect` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `princess_connect`;
+
+-- 傾印  資料表 princess_connect.group 結構
+CREATE TABLE IF NOT EXISTS `group` (
+  `server_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `group_serial` int(11) NOT NULL,
+  `now_week` int(10) unsigned NOT NULL,
+  `week_offset` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `week_offset_1` tinyint(3) unsigned NOT NULL DEFAULT '10',
+  `week_offset_2` tinyint(3) unsigned NOT NULL DEFAULT '5',
+  `week_offset_3` tinyint(3) unsigned NOT NULL DEFAULT '3',
+  `week_offset_4` tinyint(3) unsigned NOT NULL DEFAULT '3',
+  `week_offset_5` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `now_boss` int(10) unsigned NOT NULL,
+  `table_channel_id` bigint(20) DEFAULT NULL,
+  `sign_channel_id` bigint(20) DEFAULT NULL,
+  `log_channel_id` bigint(20) DEFAULT NULL,
+  `information_channel_id` bigint(20) DEFAULT NULL,
+  `table_message_id` bigint(20) DEFAULT NULL,
+  `knife_pool_message_id` bigint(20) DEFAULT NULL,
+  `information_message_id` bigint(20) DEFAULT NULL,
+  `controller_role_id` bigint(20) DEFAULT NULL,
+  `boss_change` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `table_style` tinyint(4) NOT NULL DEFAULT '0',
+  `upload` tinyint(1) unsigned zerofill DEFAULT '0',
+  PRIMARY KEY (`server_id`,`group_serial`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='設定檔';
+
+-- 取消選取資料匯出。
+
+-- 傾印  資料表 princess_connect.group_captain 結構
+CREATE TABLE IF NOT EXISTS `group_captain` (
+  `server_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `group_serial` int(10) unsigned NOT NULL DEFAULT '0',
+  `member_id` bigint(20) unsigned NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='戰隊隊長';
+
+-- 取消選取資料匯出。
+
+-- 傾印  資料表 princess_connect.keep_knifes 結構
+CREATE TABLE IF NOT EXISTS `keep_knifes` (
+  `serial_number` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `server_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `group_serial` int(11) unsigned NOT NULL,
+  `member_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `boss` int(11) unsigned NOT NULL,
+  `comment` varchar(150) NOT NULL DEFAULT '',
+  PRIMARY KEY (`serial_number`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=273 DEFAULT CHARSET=utf8mb4;
+
+-- 取消選取資料匯出。
+
+-- 傾印  資料表 princess_connect.knifes 結構
+CREATE TABLE IF NOT EXISTS `knifes` (
+  `serial_number` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `server_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `group_serial` tinyint(4) unsigned NOT NULL,
+  `member_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `week` tinyint(4) unsigned NOT NULL,
+  `boss` tinyint(4) unsigned NOT NULL,
+  `comment` varchar(150) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `knife_type` varchar(1) NOT NULL DEFAULT '0',
+  `real_damage` int(10) unsigned NOT NULL DEFAULT '0',
+  `damage_upto` int(10) unsigned NOT NULL DEFAULT '0',
+  `damage_downto` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`serial_number`)
+) ENGINE=InnoDB AUTO_INCREMENT=3832 DEFAULT CHARSET=utf8mb4 COMMENT='每刀資料';
+
+-- 取消選取資料匯出。
+
+-- 傾印  資料表 princess_connect.members 結構
+CREATE TABLE IF NOT EXISTS `members` (
+  `server_id` bigint(20) unsigned NOT NULL,
+  `member_id` bigint(20) NOT NULL,
+  `group_serial` bigint(20) unsigned NOT NULL,
+  `knifes` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `period` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`server_id`,`member_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- 取消選取資料匯出。
+
+-- 傾印  資料表 princess_connect.month 結構
+CREATE TABLE IF NOT EXISTS `month` (
+  `day1` timestamp NULL DEFAULT NULL,
+  `day2` timestamp NULL DEFAULT NULL,
+  `day3` timestamp NULL DEFAULT NULL,
+  `day4` timestamp NULL DEFAULT NULL,
+  `day5` timestamp NULL DEFAULT NULL,
+  `boss1_1` int(11) DEFAULT NULL,
+  `boss1_2` int(11) DEFAULT NULL,
+  `boss1_3` int(11) DEFAULT NULL,
+  `boss1_4` int(11) DEFAULT NULL,
+  `boss1_5` int(11) DEFAULT NULL,
+  `boss2_1` int(11) DEFAULT NULL,
+  `boss2_2` int(11) DEFAULT NULL,
+  `boss2_3` int(11) DEFAULT NULL,
+  `boss2_4` int(11) DEFAULT NULL,
+  `boss2_5` int(11) DEFAULT NULL,
+  `boss3_1` int(11) DEFAULT NULL,
+  `boss3_2` int(11) DEFAULT NULL,
+  `boss3_3` int(11) DEFAULT NULL,
+  `boss3_4` int(11) DEFAULT NULL,
+  `boss3_5` int(11) DEFAULT NULL,
+  `boss4_1` int(11) DEFAULT NULL,
+  `boss4_2` int(11) DEFAULT NULL,
+  `boss4_3` int(11) DEFAULT NULL,
+  `boss4_4` int(11) DEFAULT NULL,
+  `boss4_5` int(11) DEFAULT NULL,
+  `boss5_1` int(11) DEFAULT NULL,
+  `boss5_2` int(11) DEFAULT NULL,
+  `boss5_3` int(11) DEFAULT NULL,
+  `boss5_4` int(11) DEFAULT NULL,
+  `boss5_5` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='每月王的血量，開戰日期與時間';
+
+-- 取消選取資料匯出。
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
