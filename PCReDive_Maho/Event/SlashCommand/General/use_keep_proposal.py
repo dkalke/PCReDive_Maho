f7b9813f4b2448c38fe9d3f3b8ae8322b5a7090a
Fredrick9122 <88374163+Fredrick9122@users.noreply.github.com>
2021-08-05 17:30:48 +0800

    Create use_keep_proposal.py

---
+++ PCReDive_Maho/Event/SlashCommand/General/use_keep_proposal.py
@@ -0,0 +1,85 @@
+import datetime
+from discord_slash.utils.manage_commands import create_option, create_choice
+import Discord_client
+import Module.DB_control
+import Module.check_boss
+import Module.check_week
+
+@Discord_client.slash.slash( 
+             name="ukp" ,
+             description="使用保留刀",
+             options= [
+                create_option(
+                     name="序列",
+                     description="填入在保留刀表上要使用的序列",
+                     option_type=4,
+                     required=True
+                 ),
+                 create_option(
+                     name="週目",
+                     description="填入要預約的週目",
+                     option_type=4,
+                     required=True
+                 ),
+                 create_option(
+                     name="boss",
+                     description="填入要打的王",
+                     option_type=4,
+                     required=True,
+                     choices=[
+                         create_choice(name="1", value=1),create_choice(name="2", value=2),create_choice(name="3", value=3),
+                         create_choice(name="4", value=4),create_choice(name="5", value=5)
+                     ]
+                 )
+             ],
+             connector={"序列": "index", "週目": "week", "boss": "boss"}
+             )
+async def usekeepKnife(ctx, index, week, boss):
+  connection = await Module.DB_control.OpenConnection(ctx)
+  if connection:
+    cursor = connection.cursor(prepared=True)
+    sql = "SELECT  now_week, now_boss, week_offset, group_serial FROM princess_connect.group WHERE server_id = ? and sign_channel_id = ? order by group_serial limit 0, 1"
+    data = (ctx.guild.id, ctx.channel.id)
+    cursor.execute(sql, data) # 認證身分
+    row = cursor.fetchone()
+    cursor.close
+    if row:
+      group_serial = row[3]
+      if Module.check_week.Check_week((row[0], row[1], row[2]), week):
+        if Module.check_boss.Check_boss((row[0], row[1], row[2]),week, boss):  
+          cursor = connection.cursor(prepared=True)
+          sql = "SELECT serial_number, member_id, comment from princess_connect.keep_knifes where server_id=? and group_serial=? order by boss limit ?,1"
+          data = (ctx.guild.id, group_serial, index-1)
+          cursor.execute(sql, data)
+          row = cursor.fetchone()
+          cursor.close()
+          if row:
+            if ctx.author.id == row[1]:
+              # 新增刀
+              cursor = connection.cursor(prepared=True)
+              sql = "INSERT INTO princess_connect.knifes (server_id, group_serial, week, boss, member_id, comment, timestamp) VALUES (?, ?, ?, ?, ?, ?, ?)"
+              data = (ctx.guild.id, group_serial, week, boss, row[1], row[2] ,datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+              cursor.execute(sql, data)
+              cursor.close
+
+              # 刪除刀
+              cursor = connection.cursor(prepared=True)
+              sql = "DELETE from princess_connect.keep_knifes where serial_number=?"
+              data = (row[0],)
+              cursor.execute(sql, data)
+              cursor.close()
+
+              connection.commit()
+              await ctx.channel.send('第' + str(week) + '週目' + str(boss) + '王，備註:' + row[2] + '，報刀成功!')
+              await Module.Update.Update(ctx, ctx.guild.id, group_serial) # 更新刀表
+            else:
+              await ctx.channel.send('您並非該刀主人喔!')
+          else:
+            await ctx.channel.send('該刀不存在喔!')   
+        else:
+          await ctx.channel.send('該王不存在喔!')
+      else:
+        await ctx.channel.send('該週目不存在喔!')
+    else:
+      await ctx.send('這裡不是報刀頻道喔!')
+    await Module.DB_control.CloseConnection(connection, ctx)
