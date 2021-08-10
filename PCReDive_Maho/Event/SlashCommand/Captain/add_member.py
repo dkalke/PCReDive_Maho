5324829b4f87a23696f52eaf32f73bf99241490e
Hung-Yu Ke <tt960945@gmail.com>
2021-08-10 16:09:58 +0800

    預計新增資訊管理功能 - [戰隊隊長]設定資訊頻道 - [戰隊隊長]添加成員 - [戰隊隊長]移除成員 - [戰隊隊長]成員多帳號設定 - [成員]出刀偏好時段

---
+++ PCReDive_Maho/Event/SlashCommand/Captain/add_member.py
@@ -0,0 +1,3 @@
+# 將成員加入戰隊
+# 新增資料庫 table:members
+# server_id、member_id、group_serial、knifes(該帳號一天有幾刀)、period(偏好時段)
---
+++ PCReDive_Maho/Event/SlashCommand/Captain/set_info_channel_here.py
@@ -0,0 +1,25 @@
+﻿import Discord_client
+import Module.DB_control
+import Module.Authentication
+
+@Discord_client.slash.subcommand( base="captain",
+                                  name="set_info_channel_here" ,
+                                  description="將目前位置做為資訊頻道"
+                                )
+async def set_info_channel_here(ctx):
+  connection = await Module.DB_control.OpenConnection(ctx)
+  if connection:
+    row = await Module.Authentication.IsCaptain(ctx, '/captain set_info_channel_here', connection, ctx.guild.id, ctx.author.id)
+    if row:
+      group_serial = int(row[0])
+      # 寫入資料庫
+      cursor = connection.cursor(prepared=True)
+      sql = "UPDATE princess_connect.group SET info_channel_id = ? WHERE server_id = ? and group_serial = ? "
+      data = (ctx.channel.id, ctx.guild.id, group_serial)
+      cursor.execute(sql, data)
+      cursor.close
+      connection.commit()
+      await ctx.send('資訊是吧，了解!')
+      
+            
+    await Module.DB_control.CloseConnection(connection, ctx)
