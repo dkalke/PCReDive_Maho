67f566aba2e9739e98c01ba13f51d51d90444a52
Hung-Yu Ke <tt960945@gmail.com>
2021-08-11 19:00:33 +0800

    進階出刀指令

---
+++ PCReDive_Maho/Event/SlashCommand/General/advanced_proposal_knife.py
@@ -0,0 +1,77 @@
+﻿from discord_slash.utils.manage_commands import create_option, create_choice
+import Discord_client
+import Module.DB_control
+import Module.check_boss
+import Module.check_week
+import Module.Update
+
+@Discord_client.slash.slash( 
+             name="ap" ,
+             description="[進階]預約打王",
+             options= [
+                 create_option(
+                     name="週目",
+                     description="填入要預約的週目",
+                     option_type=4,
+                     required=True
+                 ),
+                 create_option(
+                     name="boss",
+                     description="填入要打的王",
+                     option_type=4,
+                     required=True,
+                     choices=[
+                         create_choice(name="1", value=1),create_choice(name="2", value=2),create_choice(name="3", value=3),
+                         create_choice(name="4", value=4),create_choice(name="5", value=5)
+                     ]
+                 ),
+                 create_option(
+                     name="類型",
+                     description="是正刀、尾刀、還是補償刀?",
+                     option_type=4,
+                     required=True,
+                     choices=[
+                         create_choice(name="正刀", value=1),create_choice(name="尾刀", value=2),create_choice(name="補償刀", value=3)
+                         # value = 0 為一般報刀使用，無法統計刀數
+                         # 尾刀出完扣除0.5
+                         # 補償刀出完扣除0.5
+                     ]
+                 ),
+                 create_option(
+                     name="備註",
+                     description="備註（預計傷害）",
+                     option_type=3,
+                     required=True
+                 )
+             ],
+             connector={"週目": "week","boss": "boss", "類型":"knife_type", "備註": "comment"}
+             )
+async def advanced_proposal_knife(ctx, week, boss, knife_type, comment):
+  connection = await Module.DB_control.OpenConnection(ctx)
+  if connection:
+    cursor = connection.cursor(prepared=True)
+    sql = "SELECT now_week, now_boss, week_offset, group_serial FROM princess_connect.group WHERE server_id = ? and sign_channel_id = ? order by group_serial limit 0, 1"
+    data = (ctx.guild.id, ctx.channel.id)
+    cursor.execute(sql, data) # 認證身分
+    row = cursor.fetchone()
+    cursor.close
+    if row:
+      group_serial = row[3]
+      if Module.check_week.Check_week((row[0], row[1], row[2]), week):
+        if Module.check_boss.Check_boss((row[0], row[1], row[2]),week, boss):
+          # 新增刀
+          cursor = connection.cursor(prepared=True)
+          sql = "INSERT INTO princess_connect.knifes (server_id, group_serial, week, boss, knife_type, member_id, comment) VALUES (?, ?, ?, ?, ?, ?, ?)"
+          data = (ctx.guild.id, group_serial, week, boss, knife_type, ctx.author.id, comment)
+          cursor.execute(sql, data)
+          cursor.close
+          connection.commit()
+          await ctx.send('第' + str(week) + '週目' + str(boss) + '王，備註:' + comment + '，報刀成功!')
+          await Module.Update.Update(ctx, ctx.guild.id, group_serial) # 更新刀表
+        else:
+          await ctx.send('該王不存在喔!')
+      else:
+        await ctx.send('該週目不存在喔!')
+    else:
+      await ctx.send('這裡不是報刀頻道喔!')
+    await Module.DB_control.CloseConnection(connection, ctx)
