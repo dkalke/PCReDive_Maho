f5b81e2235b18e3199be116b82d2a0ed2521dbfd
Hung-Yu Ke <tt960945@gmail.com>
2021-08-22 03:40:35 +0800

    僅有強制回報傷害的戰隊才需使用此指令

--- PCReDive_Maho/Event/SlashCommand/General/done_proposal.py
+++ PCReDive_Maho/Event/SlashCommand/General/done_proposal.py
@@ -4,6 +4,7 @@ import Discord_client
 import Module.DB_control
 import Module.Update
 import Module.info_update
+import Module.define_value
 
 @Discord_client.slash.slash( 
              name="d" ,
@@ -40,7 +41,7 @@ async def done_proposal(ctx, index, knife_type, real_damage):
   connection = await Module.DB_control.OpenConnection(ctx)
   if connection:
     cursor = connection.cursor(prepared=True)
-    sql = "SELECT now_week, now_boss, week_offset, group_serial FROM princess_connect.group WHERE server_id = ? and sign_channel_id = ? order by group_serial limit 0, 1"
+    sql = "SELECT now_week, now_boss, week_offset, group_serial, policy FROM princess_connect.group WHERE server_id = ? and sign_channel_id = ? order by group_serial limit 0, 1"
     data = (ctx.guild.id, ctx.channel.id)
     cursor.execute(sql, data) # 認證身分
     row = cursor.fetchone()
@@ -49,31 +50,35 @@ async def done_proposal(ctx, index, knife_type, real_damage):
       now_week = row[0]
       now_boss = row[1]
       group_serial = row[3]
-      if index > 0:
-        # 尋找該刀
-        cursor = connection.cursor(prepared=True)
-        sql = "SELECT serial_number,member_id from princess_connect.knifes where server_id=? and group_serial=? and week=? and boss=? order by serial_number limit ?,1"
-        data = (ctx.guild.id, group_serial, now_week, now_boss, index-1)
-        cursor.execute(sql, data)
-        row = cursor.fetchone()
-        cursor.close()
-        if row:
-          if ctx.author.id == row[1]:
-            cursor = connection.cursor(prepared=True)
-            sql = "UPDATE princess_connect.knifes set real_damage=?, knife_type=?, done_time=? where serial_number=?"
-            data = (real_damage, knife_type, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), row[0])
-            row = cursor.execute(sql, data)
-            cursor.close()
-            connection.commit()
-            await ctx.send('回報成功!')
-            await Module.Update.Update(ctx, ctx.guild.id, group_serial) # 更新刀表
-            await Module.info_update.info_update(ctx ,ctx.guild.id, group_serial) # 更新資訊
+      policy = row[4]
+      if policy == Module.define_value.Policy.YES.value:
+        if index > 0:
+          # 尋找該刀
+          cursor = connection.cursor(prepared=True)
+          sql = "SELECT serial_number,member_id from princess_connect.knifes where server_id=? and group_serial=? and week=? and boss=? order by serial_number limit ?,1"
+          data = (ctx.guild.id, group_serial, now_week, now_boss, index-1)
+          cursor.execute(sql, data)
+          row = cursor.fetchone()
+          cursor.close()
+          if row:
+            if ctx.author.id == row[1]:
+              cursor = connection.cursor(prepared=True)
+              sql = "UPDATE princess_connect.knifes set real_damage=?, knife_type=?, done_time=? where serial_number=?"
+              data = (real_damage, knife_type, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), row[0])
+              row = cursor.execute(sql, data)
+              cursor.close()
+              connection.commit()
+              await ctx.send('回報成功!')
+              await Module.Update.Update(ctx, ctx.guild.id, group_serial) # 更新刀表
+              await Module.info_update.info_update(ctx ,ctx.guild.id, group_serial) # 更新資訊
+            else:
+              await ctx.send('您並非該刀主人喔!')
           else:
-            await ctx.send('您並非該刀主人喔!')
+            await ctx.send('該刀不存在喔!')
         else:
-          await ctx.send('該刀不存在喔!')
+          await ctx.send('序號必須大於0!')
       else:
-        await ctx.send('序號必須大於0!')
+        await ctx.send('目前戰隊政策為:**不回報傷害**! 指令無效，感謝你的自主回報!')
         
     else:
       await ctx.send('這裡不是報刀頻道喔!')
