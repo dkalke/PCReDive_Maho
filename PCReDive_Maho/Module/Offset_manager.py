2ceac5c12d05589c75783a043975a8e5cf07d51f
Hung-Yu Ke <tt960945@gmail.com>
2021-10-02 10:49:57 +0800

    階段週目調整

--- PCReDive_Maho/Module/Offset_manager.py
+++ PCReDive_Maho/Module/Offset_manager.py
@@ -1,9 +1,9 @@
 def AutoOffset(connection, server_id, group_serial):  
   # 一階段:1~3周
   # 二階段:4~10周
-  # 三階段:11~34周
-  # 四階段:35~44周
-  # 五階段:45周~
+  # 三階段:11~30周
+  # 四階段:31~40周
+  # 五階段:41周~
 
   cursor = connection.cursor(prepared=True)
   sql = "SELECT now_week, week_offset, week_offset_1, week_offset_2, week_offset_3, week_offset_4, week_offset_5 FROM princess_connect.group WHERE server_id = ? and group_serial = ? limit 0, 1"
@@ -17,7 +17,7 @@ def AutoOffset(connection, server_id, group_serial):
     now_week = row[0]
     week_offset = row[1]
     level_offset = [row[2], row[3], row[4], row[5], row[6]]
-    week_threshold = [1,4,11,35,45]
+    week_threshold = [1,4,11,31,41]
 
     # 目前的最大可視範圍
     if week_threshold[0] <= now_week and now_week < week_threshold[1]:
