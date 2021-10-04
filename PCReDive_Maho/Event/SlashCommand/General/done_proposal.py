80d4109eaad5c6e3c2d1fd14fcb2e22099e6b441
Hung-Yu Ke <tt960945@gmail.com>
2021-10-04 22:18:58 +0800

    因應自由出刀修正指令

--- PCReDive_Maho/Event/SlashCommand/General/done_proposal.py
+++ PCReDive_Maho/Event/SlashCommand/General/done_proposal.py
@@ -11,6 +11,16 @@ import Module.define_value
              description="出刀回報，當出完刀表上的某一刀時使用",
              options= [
                  create_option(
+                    name="boss",
+                    description="填入要打的王",
+                    option_type=4,
+                    required=True,
+                    choices=[
+                        create_choice(name="1", value=1),create_choice(name="2", value=2),create_choice(name="3", value=3),
+                        create_choice(name="4", value=4),create_choice(name="5", value=5)
+                    ]
+                 ),
+                 create_option(
                      name="序號",
                      description="請查看刀表，填入該刀在刀表中的序號",
                      option_type=4,
@@ -35,28 +45,27 @@ import Module.define_value
                      required=True
                  )
              ],
-             connector={"序號": "index", "類型": "knife_type", "傷害": "real_damage"}
+             connector={"boss":"boss", "序號": "index", "類型": "knife_type", "傷害": "real_damage"}
              )
-async def done_proposal(ctx, index, knife_type, real_damage):
+async def done_proposal(ctx, boss, index, knife_type, real_damage):
   connection = await Module.DB_control.OpenConnection(ctx)
   if connection:
     cursor = connection.cursor(prepared=True)
-    sql = "SELECT now_week, now_boss, week_offset, group_serial, policy FROM princess_connect.group WHERE server_id = ? and sign_channel_id = ? order by group_serial limit 0, 1"
+    sql = "SELECT now_week_1, now_week_2, now_week_3, now_week_4, now_week_5, group_serial, policy FROM princess_connect.group WHERE server_id = ? and sign_channel_id = ? order by group_serial limit 0, 1"
     data = (ctx.guild.id, ctx.channel.id)
     cursor.execute(sql, data) # 認證身分
     row = cursor.fetchone()
     cursor.close
     if row:
-      now_week = row[0]
-      now_boss = row[1]
-      group_serial = row[3]
-      policy = row[4]
+      now_week = [row[0], row[1], row[2], row[3], row[4]]
+      group_serial = row[5]
+      policy = row[6]
       if policy == Module.define_value.Policy.YES.value:
         if index > 0:
           # 尋找該刀
           cursor = connection.cursor(prepared=True)
           sql = "SELECT serial_number,member_id from princess_connect.knifes where server_id=? and group_serial=? and week=? and boss=? order by serial_number limit ?,1"
-          data = (ctx.guild.id, group_serial, now_week, now_boss, index-1)
+          data = (ctx.guild.id, group_serial, now_week[boss-1], boss, index-1)
           cursor.execute(sql, data)
           row = cursor.fetchone()
           cursor.close()
