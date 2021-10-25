7112e4aabe97f930a6d23cce314c77762d2333a5
Hung-Yu Ke <tt960945@gmail.com>
2021-10-25 15:56:26 +0800

    - 增加戰隊列表所顯示的資訊

--- PCReDive_Maho/Event/SlashCommand/Admin/group_list.py
+++ PCReDive_Maho/Event/SlashCommand/Admin/group_list.py
@@ -5,6 +5,7 @@ import Discord_client
 import Module.DB_control
 import Module.Authentication
 import Name_manager
+import Module.define_value
 
 @Discord_client.slash.subcommand( base="admin",
                                   name="group_list" ,
@@ -19,7 +20,7 @@ async def group_list(ctx):
       if connection2:
         # 列出所有戰隊
         cursor = connection.cursor(prepared=True)
-        sql = "SELECT server_id, group_serial, controller_role_id, table_channel_id, sign_channel_id FROM princess_connect.group WHERE server_id = ? order by group_serial"
+        sql = "SELECT server_id, group_serial, controller_role_id, table_channel_id, sign_channel_id, policy, week_offset_1, week_offset_2, week_offset_3, week_offset_4, week_offset_5,info_channel_id FROM princess_connect.group WHERE server_id = ? order by group_serial"
         data = (ctx.guild.id, )
         cursor.execute(sql, data)
         row = cursor.fetchone()
@@ -33,13 +34,35 @@ async def group_list(ctx):
           controller_role_id = row[2]
           table_channel_id = row[3]
           sign_channel_id = row[4]
-                
+          policy = row[5]
+          week_offsets = [row[6], row[7], row[8], row[9], row[10]]
+          info_channel_id = row[11]
+          
+          # 戰隊政策
+          policy_msg = ''
+          if policy == Module.define_value.Policy.YES.value:
+            policy_msg = '需回報傷害'
+          elif policy == Module.define_value.Policy.NO.value:
+            policy_msg = '不回報傷害(部分功能無法使用)'
+          else:
+            policy_msg = '[N/A]'
+
+          # 提前週目設定
+          week_offset_msg = \
+            '1階' + str(week_offsets[0]) + \
+            '週、2階' + str(week_offsets[1]) + \
+            '週、3階' + str(week_offsets[2]) + \
+            '週、4階' + str(week_offsets[3]) + \
+            '週、5階' + str(week_offsets[4]) + \
+            '週'
+
+
           # 找出刀表頻道
           table_msg = ''
           if not table_channel_id == None:
             try:
               channel = ctx.guild.get_channel(table_channel_id)
-              table_msg = channel.name
+              table_msg = channel.mention
             except:
               print("!戰隊列表查無刀表頻道")
               table_msg = '[N/A]'
@@ -51,10 +74,22 @@ async def group_list(ctx):
           if not sign_channel_id == None:
             try:
               channel = ctx.guild.get_channel(sign_channel_id)
-              sign_msg = channel.name
+              sign_msg = channel.mention
             except:
               print("!戰隊列表查無報刀頻道")
               sign_msg = '[N/A]'
+          else:
+            pass
+
+          # 找出資訊頻道
+          info_msg = ''
+          if not info_channel_id == None:
+            try:
+              channel = ctx.guild.get_channel(info_channel_id)
+              info_msg = channel.mention
+            except:
+              print("!戰隊列表查無資訊頻道")
+              info_msg = '[N/A]'
           else:
             pass
 
@@ -86,25 +121,32 @@ async def group_list(ctx):
 
           # 組裝
           msg_send = ''
-          if table_msg == '':
-            msg_send = msg_send + '刀表頻道:尚未指派!\n'
+          if captain_msg == '':
+            msg_send = msg_send + '隊長:尚未指派!\n'
           else:
-            msg_send = msg_send + '刀表頻道:'+ table_msg +'\n'
-
-          if sign_msg == '':
-            msg_send = msg_send + '刀表頻道:尚未指派!\n'
-          else:
-            msg_send = msg_send + '刀表頻道:'+ sign_msg +'\n'
+            msg_send = msg_send + '隊長:' + captain_msg + '\n'
 
           if controller_role_msg == '':
             msg_send = msg_send + '控刀手身分組:尚未指派!\n'
           else:
             msg_send = msg_send + '控刀手身分組:'+ controller_role_msg +'\n'
 
-          if captain_msg == '':
-            msg_send = msg_send + '隊長:尚未指派!\n'
+          msg_send = msg_send + '戰隊政策:' + policy_msg + '\n' + '提前週目數:' + week_offset_msg + '\n'
+
+          if table_msg == '':
+            msg_send = msg_send + '刀表頻道:尚未指派!\n'
           else:
-            msg_send = msg_send + '隊長:' + captain_msg + '\n'
+            msg_send = msg_send + '刀表頻道:'+ table_msg +'\n'
+
+          if sign_msg == '':
+            msg_send = msg_send + '報刀頻道:尚未指派!\n'
+          else:
+            msg_send = msg_send + '報刀頻道:'+ sign_msg +'\n'
+
+          if info_msg == '':
+            msg_send = msg_send + '資訊頻道:尚未指派!\n'
+          else:
+            msg_send = msg_send + '資訊頻道:'+ info_msg +'\n'
 
           embed_msg.add_field(name='第' + str(group_serial) + '戰隊:', value=msg_send, inline=False)
           row = cursor.fetchone()
