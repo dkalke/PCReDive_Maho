41f65b23f8ac124aae3fd4a31fd39404bdecccc8
Hung-Yu Ke <tt960945@gmail.com>
2021-11-22 19:49:41 +0800

    /controller set_progress(週目調整功能)改為一次調整五週目

--- PCReDive_Maho/Event/SlashCommand/Controller/set_progress.py
+++ PCReDive_Maho/Event/SlashCommand/Controller/set_progress.py
@@ -12,53 +12,59 @@ import Module.Update
                                   description="強制切換至指定boss的週目",
                                   options=[
                                     create_option(
-                                      name="boss",
-                                      description="哪隻boss?",
-                                      option_type=4,
-                                      required=True,
-                                      choices=[
-                                        create_choice(name="1", value=1),
-                                        create_choice(name="2", value=2),
-                                        create_choice(name="3", value=3),
-                                        create_choice(name="4", value=4),
-                                        create_choice(name="5", value=5),
-                                      ]
-                                    ),
-                                    create_option(
-                                      name="週目",
-                                      description="第幾週?",
+                                      name="1王週目",
+                                      description="1王要設定成第幾週?",
                                       option_type=4,
                                       required=True
-                                    )
+                                    ),
+                                    create_option(
+                                      name="2王週目",
+                                      description="2王要設定成第幾週?",
+                                      option_type=4,
+                                      required=True
+                                    ),
+                                    create_option(
+                                      name="3王週目",
+                                      description="3王要設定成第幾週?",
+                                      option_type=4,
+                                      required=True
+                                    ),
+                                    create_option(
+                                      name="4王週目",
+                                      description="4王要設定成第幾週?",
+                                      option_type=4,
+                                      required=True
+                                    ),
+                                    create_option(
+                                      name="5王週目",
+                                      description="5王要設定成第幾週?",
+                                      option_type=4,
+                                      required=True
+                                    ),
                                   ],
-                                  connector={"boss": "boss", "週目":"week"}
+                                  connector={"1王週目":"week1", "2王週目":"week2", "3王週目":"week3", "4王週目":"week4", "5王週目":"week5"}
                                 )
-async def set_progress(ctx, boss, week):
+async def set_progress(ctx, week1, week2, week3, week4, week5):
   # check身分，並找出所屬組別
   connection = await Module.DB_control.OpenConnection(ctx)
   if connection:
     ( main_week, now_week, week_offset, group_serial ) = await Module.Authentication.IsController(ctx ,'/controller set_progress', connection, ctx.guild.id)
     if not group_serial == 0: # 如果是是控刀手
-      if week > 0:
+      if week1 > 0 and week2 > 0 and week3 > 0 and week4 > 0 and week5 > 0:
         cursor = connection.cursor(prepared=True)
         sql = ""
-        now_week[boss-1] = week
-        main_week = min(now_week)
-        if boss == 1:
-          sql = "UPDATE princess_connect.group SET now_week=?, now_week_1=? WHERE server_id = ? and group_serial=? order by group_serial"
-        elif boss == 2:
-          sql = "UPDATE princess_connect.group SET now_week=?, now_week_2=? WHERE server_id = ? and group_serial=? order by group_serial"
-        elif boss == 3:
-          sql = "UPDATE princess_connect.group SET now_week=?, now_week_3=? WHERE server_id = ? and group_serial=? order by group_serial"
-        elif boss == 4:
-          sql = "UPDATE princess_connect.group SET now_week=?, now_week_4=? WHERE server_id = ? and group_serial=? order by group_serial"
-        elif boss == 5:
-          sql = "UPDATE princess_connect.group SET now_week=?, now_week_5=? WHERE server_id = ? and group_serial=? order by group_serial"
-        data = (main_week, week, ctx.guild.id, group_serial)
+        sql = "UPDATE princess_connect.group SET now_week=?, now_week_1=?, now_week_2=?, now_week_3=?, now_week_4=?, now_week_5=? WHERE server_id = ? and group_serial=? order by group_serial"
+        
+        data = (min(week1, week2, week3, week4, week5), week1, week2, week3, week4, week5, ctx.guild.id, group_serial)
         cursor.execute(sql, data)
         cursor.close
         connection.commit()
-        await ctx.send('已切換' + str(boss) + '王至'+ str(week) + '週目!')
+        await ctx.send('週目已切換，詳細資訊如下:\n' + 
+                       '1王:'+ str(week1) + '週目!\n' + 
+                       '2王:'+ str(week2) + '週目!\n' + 
+                       '3王:'+ str(week3) + '週目!\n' + 
+                       '4王:'+ str(week4) + '週目!\n' + 
+                       '5王:'+ str(week5) + '週目!\n')
         Module.Offset_manager.AutoOffset(connection, ctx.guild.id, group_serial)
         await Module.Update.Update(ctx, ctx.guild.id, group_serial) # 更新刀表
       else:
