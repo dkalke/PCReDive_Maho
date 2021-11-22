4c9d19027c0b2b35784ed9eb6a2432f434fbae43
Hung-Yu Ke <tt960945@gmail.com>
2021-11-22 18:55:48 +0800

    DB結構更新

--- PCReDive_Maho/init-files/priceseDB.sql
+++ PCReDive_Maho/init-files/priceseDB.sql
@@ -7,12 +7,10 @@
 
 
 -- 傾印 princess_connect 的資料庫結構
-DROP DATABASE IF EXISTS `princess_connect`;
 CREATE DATABASE IF NOT EXISTS `princess_connect` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `princess_connect`;
 
 -- 傾印  資料表 princess_connect.group 結構
-DROP TABLE IF EXISTS `group`;
 CREATE TABLE IF NOT EXISTS `group` (
   `server_id` bigint(20) unsigned NOT NULL DEFAULT 0,
   `group_serial` int(11) NOT NULL,
@@ -45,14 +43,13 @@ CREATE TABLE IF NOT EXISTS `group` (
   `boss_change_5` datetime NOT NULL DEFAULT current_timestamp(),
   `table_style` tinyint(4) NOT NULL DEFAULT 0,
   `upload` tinyint(1) unsigned zerofill DEFAULT 0,
-  `policy` tinyint(1) unsigned NOT NULL DEFAULT 0,
+  `policy` int(10) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`server_id`,`group_serial`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='設定檔';
 
 -- 取消選取資料匯出。
 
 -- 傾印  資料表 princess_connect.group_captain 結構
-DROP TABLE IF EXISTS `group_captain`;
 CREATE TABLE IF NOT EXISTS `group_captain` (
   `server_id` bigint(20) unsigned NOT NULL DEFAULT 0,
   `group_serial` int(10) unsigned NOT NULL DEFAULT 0,
@@ -62,21 +59,18 @@ CREATE TABLE IF NOT EXISTS `group_captain` (
 -- 取消選取資料匯出。
 
 -- 傾印  資料表 princess_connect.keep_knifes 結構
-DROP TABLE IF EXISTS `keep_knifes`;
 CREATE TABLE IF NOT EXISTS `keep_knifes` (
   `serial_number` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `server_id` bigint(20) unsigned NOT NULL DEFAULT 0,
   `group_serial` int(11) unsigned NOT NULL,
   `member_id` bigint(20) unsigned NOT NULL DEFAULT 0,
-  `boss` int(11) unsigned NOT NULL,
   `comment` varchar(150) NOT NULL DEFAULT '',
   PRIMARY KEY (`serial_number`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=333 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=307 DEFAULT CHARSET=utf8mb4;
 
 -- 取消選取資料匯出。
 
 -- 傾印  資料表 princess_connect.knifes 結構
-DROP TABLE IF EXISTS `knifes`;
 CREATE TABLE IF NOT EXISTS `knifes` (
   `serial_number` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `server_id` bigint(20) unsigned NOT NULL DEFAULT 0,
@@ -90,12 +84,11 @@ CREATE TABLE IF NOT EXISTS `knifes` (
   `knife_type` tinyint(4) NOT NULL DEFAULT 0,
   `real_damage` int(10) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`serial_number`)
-) ENGINE=InnoDB AUTO_INCREMENT=6775 DEFAULT CHARSET=utf8mb4 COMMENT='每刀資料';
+) ENGINE=InnoDB AUTO_INCREMENT=5412 DEFAULT CHARSET=utf8mb4 COMMENT='每刀資料';
 
 -- 取消選取資料匯出。
 
 -- 傾印  資料表 princess_connect.members 結構
-DROP TABLE IF EXISTS `members`;
 CREATE TABLE IF NOT EXISTS `members` (
   `server_id` bigint(20) unsigned NOT NULL,
   `member_id` bigint(20) NOT NULL,
@@ -108,7 +101,6 @@ CREATE TABLE IF NOT EXISTS `members` (
 -- 取消選取資料匯出。
 
 -- 傾印  資料表 princess_connect.month 結構
-DROP TABLE IF EXISTS `month`;
 CREATE TABLE IF NOT EXISTS `month` (
   `day1` timestamp NULL DEFAULT NULL,
   `day2` timestamp NULL DEFAULT NULL,
