1743d50aecec32256bab6b134413492ae161f95d
Hung-Yu Ke <tt960945@gmail.com>
2021-12-12 18:33:27 +0800

    取得最接近的結束時間

---
+++ PCReDive_Maho/Module/get_closest_end_time.py
@@ -0,0 +1,5 @@
+import datetime
+def get_closest_day_end(in_time: datetime) -> datetime:
+  if in_time.hour >= 5:
+    in_time = in_time + datetime.timedelta(days = 1)
+  return datetime.datetime(in_time.year,in_time.month,in_time.day,5)
