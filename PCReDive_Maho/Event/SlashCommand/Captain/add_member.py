5a5c86505e96ce279a0e4de3a3fededf7ead5785
宏諭 柯 <tt960945@gmail.com>
2022-02-18 00:36:08 +0800

    檢查指令頻道，優化效能

--- PCReDive_Maho/Event/SlashCommand/Captain/add_member.py
+++ PCReDive_Maho/Event/SlashCommand/Captain/add_member.py
@@ -31,25 +31,24 @@ async def add_captain(ctx, member):
     row = await Module.Authentication.IsCaptain(ctx ,'/captain add_member', connection, ctx.guild.id, ctx.author.id)
     if row:
       group_serial = row[0]
-
-      # 檢查成員是否已存在戰隊中
-      cursor = connection.cursor(prepared=True)
-      sql = "SELECT * FROM princess_connect.members WHERE server_id=? and member_id=? and group_serial=? LIMIT 0, 1"
-      data = (ctx.guild.id, member.id, group_serial)
-      cursor.execute(sql, data)
-      row = cursor.fetchone()
-      cursor.close
-      if not row:
-        # 寫入成員名單
+      if await Module.Authentication.IsSignChannel(ctx, connection, group_serial):# 是否位於報刀頻道
+        # 檢查成員是否已存在戰隊中
         cursor = connection.cursor(prepared=True)
-        sql = "INSERT INTO princess_connect.members (server_id, group_serial, member_id, knifes, period, last_sl_time) VALUES (?, ?, ?, ?, ?, ?)"
-        data = (ctx.guild.id, group_serial, member.id, 3, Module.define_value.Period.UNKNOW.value, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) # 預設三刀
+        sql = "SELECT 1 FROM princess_connect.members WHERE server_id=? and member_id=? and group_serial=? LIMIT 0, 1"
+        data = (ctx.guild.id, member.id, group_serial)
         cursor.execute(sql, data)
+        row = cursor.fetchone()
+        if not row:
+          # 寫入成員名單
+          sql = "INSERT INTO princess_connect.members (server_id, group_serial, member_id, knifes, period, last_sl_time) VALUES (?, ?, ?, ?, ?, ?)"
+          data = (ctx.guild.id, group_serial, member.id, 3, Module.define_value.Period.UNKNOW.value, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")) # 預設三刀
+          cursor.execute(sql, data)
+        
+          connection.commit() # 資料庫存檔
+          await Module.info_update.info_update(ctx ,ctx.guild.id, group_serial)
+          await ctx.send( member.name + ' 已新增為第' + str(group_serial) + '戰隊成員。')
+        else:
+          await ctx.send( member.name + ' 目前已為第' + str(group_serial) + '戰隊成員。')
         cursor.close
-        connection.commit() # 資料庫存檔
-        await Module.info_update.info_update(ctx ,ctx.guild.id, group_serial)
-        await ctx.send( member.name + ' 已新增為第' + str(group_serial) + '戰隊成員。')
-      else:
-        await ctx.send( member.name + ' 目前已為第' + str(group_serial) + '戰隊成員。')
 
       await Module.DB_control.CloseConnection(connection, ctx)
