e3a4f685421260cd02466e2d3a509864cf49725f
宏諭 柯 <tt960945@gmail.com>
2022-02-18 23:26:57 +0800

    資訊表改版，更動取得時間邏輯

---
+++ PCReDive_Maho/Module/half_string_to_full.py
@@ -0,0 +1,11 @@
+﻿# 半形轉全形
+def half_string_to_full(s):
+  rstring = ""
+  for uchar in s:
+      u_code = ord(uchar)
+      if u_code == 32:  # 全形空格直接轉換
+          u_code = 12288
+      elif 33 <= u_code <= 126:  # 全形字元（除空格）根據關係轉化
+          u_code += 65248
+      rstring += chr(u_code)
+  return rstring
--- PCReDive_Maho/Module/info_update.py
+++ PCReDive_Maho/Module/info_update.py
@@ -6,6 +6,7 @@ import Name_manager
 import Module.DB_control
 import Module.define_value
 import Module.get_closest_end_time
+import Module.half_string_to_full
 
 async def info_update(message ,server_id, group_serial):
   # 取得資訊訊息物件
@@ -14,7 +15,7 @@ async def info_update(message ,server_id, group_serial):
     connection = await Module.DB_control.OpenConnection(message)
     if connection.is_connected():
       cursor = connection.cursor(prepared=True)
-      sql = "SELECT info_channel_id, info_message_id, policy FROM princess_connect.group WHERE server_id = ? and group_serial = ? LIMIT 0, 1"
+      sql = "SELECT info_channel_id, info_message_id FROM princess_connect.group WHERE server_id = ? and group_serial = ? LIMIT 0, 1"
       data = (server_id, group_serial)
       cursor.execute(sql, data)
       row = cursor.fetchone()
@@ -22,69 +23,61 @@ async def info_update(message ,server_id, group_serial):
       if row: # 戰隊存在
         info_channel_id = row[0]
         info_message_id = row[1]
-        policy = row[2]
 
         if info_message_id :
           # 取得戰隊戰日期
           date_now = datetime.datetime.now()
-          cursor = connection.cursor(prepared=True)
-          sql = "SELECT day1, day2, day3, day4, day5, day6 FROM princess_connect.month LIMIT 0, 1"
-          cursor.execute(sql)
-          row = cursor.fetchone()
-          day = [row[0], row[1], row[2], row[3], row[4], row[5]]
-          day_name = ''
-          cls_time_start = day[0]
-          fetch_start = None
-          fetch_end = None
+          end_time = Module.get_closest_end_time.get_closest_end_time(date_now) # 取得當日結束時間 29點00分
+          start_time = end_time - datetime.timedelta(days=1)
 
-          if date_now < day[0]:
+          day_name = ''
+          if start_time < Module.define_value.BATTLE_DAY[0]:
             day_name = '(尚未開戰)'
-          elif date_now < day[1]:
+          elif start_time == Module.define_value.BATTLE_DAY[0]:
             day_name = '戰隊戰第1天'
-            fetch_start = day[0]
-            fetch_end = day[1]
-          elif date_now < day[2]:
+          elif start_time == Module.define_value.BATTLE_DAY[1]:
             day_name = '戰隊戰第2天'
-            fetch_start = day[1]
-            fetch_end = day[2]
-          elif date_now < day[3]:
+          elif start_time == Module.define_value.BATTLE_DAY[2]:
             day_name = '戰隊戰第3天'
-            fetch_start = day[2]
-            fetch_end = day[3]
-          elif date_now < day[4]:
+          elif start_time == Module.define_value.BATTLE_DAY[3]:
             day_name = '戰隊戰第4天'
-            fetch_start = day[3]
-            fetch_end = day[4]
-          elif date_now < day[4]:
+          elif start_time == Module.define_value.BATTLE_DAY[4]:
             day_name = '戰隊戰第5天'
-            fetch_start = day[4]
-            fetch_end = day[5]
           else:
             day_name = '(等待下月戰隊戰日期公佈中)'
-
-          closest_end_time = Module.get_closest_end_time.get_closest_day_end(date_now) # 取得當日結束時間 29點00分
-
           embed_msg = Embed(title='第' + str(group_serial) + '戰隊資訊', description=day_name ,color=0xD98B99)
-
-
 
           # 列出序號、成員名稱、剩餘刀數、出刀偏好
           cursor = connection.cursor(prepared=True)
-          sql = "SELECT member_id, knifes, period, last_sl_time FROM princess_connect.members WHERE server_id = ? and group_serial = ?"
+          sql = "SELECT a.serial_number, a.member_id, a.period, a.sockpuppet, a.last_sl_time, b.normal, b.reserved\
+                  FROM princess_connect.members a\
+                  LEFT JOIN princess_connect.knife_summary b ON a.serial_number = b.serial_number\
+                  WHERE server_id = ?  and group_serial = ?"
           data = (server_id, group_serial)
           cursor.execute(sql, data)
           row = cursor.fetchone()
           msg = ''
           count = 1
+          normal_total = 0 # 已出正刀數量合計
+          reversed_total = 0 # 已出補償刀數量合計
           while row:
-            member_id=row[0]
-            knifes=row[1]
-            period=row[2]
-            # 是否有SL?
-            sl_condition = ':ballot_box_with_check: '
-            if row[3] >= closest_end_time:
-              sl_condition = ':negative_squared_cross_mark: '
+            serial_number = row[0]
+            member_id = row[1]
+            member_name = await Name_manager.get_nick_name(message, member_id)
 
+            period = row[2]
+            sockpuppet = row[3]
+            # 是否有SL?
+            have_sl = 'Y'
+            if row[4] >= end_time:
+              have_sl = 'N'
+
+            normal = 0
+            if row[5] != None:
+              normal = row[5]
+            reserved = 0
+            if row[6] != None:
+              reserved = row[5]
             if period == Module.define_value.Period.UNKNOW.value:
               period = '不定'
             elif period == Module.define_value.Period.DAY.value:
@@ -98,31 +91,11 @@ async def info_update(message ,server_id, group_serial):
             else:
               period = '錯誤'
 
-            knifes_msg = ''
-            # 已出刀數計算，僅支援開啟回報傷害戰隊使用
-            if policy == Module.define_value.Policy.YES.value and fetch_start and fetch_end :
+           
+            normal_total += normal
+            reversed_total += reserved
+            msg = msg + '（{}）　{}　{}正{}補　{}　{}-{}\n'.format(Module.half_string_to_full.half_string_to_full('{:>02d}'.format(count)), period, Module.half_string_to_full.half_string_to_full(str(normal)), Module.half_string_to_full.half_string_to_full(str(reserved)),Module.half_string_to_full.half_string_to_full(have_sl), Module.half_string_to_full.half_string_to_full(str(sockpuppet)), member_name) 
 
-              cursor2 = connection2.cursor(prepared=True)
-              sql = "SELECT knife_type FROM princess_connect.knifes WHERE server_id = ? and group_serial = ? and member_id = ? and ? < done_time and done_time < ?"
-              data = (server_id, group_serial, member_id, fetch_start, fetch_end)
-              cursor2.execute(sql, data)
-              in_row = cursor2.fetchone()
-              done_knifes = 0
-              while in_row:
-                if in_row[0] == 1: # 正刀
-                  done_knifes = done_knifes + 1 
-                elif in_row[0] == 2: # 尾刀
-                  done_knifes = done_knifes + 0.5
-                elif in_row[0] == 3: # 補償刀
-                  done_knifes = done_knifes + 0.5
-                in_row = cursor2.fetchone()
-              cursor2.close
-              knifes_msg = sl_condition + '持有' + str(knifes) + '刀，剩餘' + '{:g}'.format(knifes - done_knifes) + '刀，'
-
-
-            member_name = await Name_manager.get_nick_name(message, member_id)
-            msg = msg + '{' + str(count) + '} ' + member_name + '\n'
-            msg = msg+ '　' + knifes_msg + '出刀偏好:' + period + '\n'
             count = count + 1
             row = cursor.fetchone()
 
@@ -131,7 +104,8 @@ async def info_update(message ,server_id, group_serial):
           if msg == '':
             msg = '尚無成員資訊!'
           else:
-            pass
+            msg = '（序號）　偏好　已出刀數　Ｒ　名稱\n-------------------------------------------\n' + msg + '-------------------------------------------\n'
+            msg = msg + '（總計）　　　　{}正{}補'.format(Module.half_string_to_full.half_string_to_full(str(normal_total)), Module.half_string_to_full.half_string_to_full(str(reversed_total)))
 
           embed_msg.add_field(name='\u200b', value=msg , inline=False)
 
