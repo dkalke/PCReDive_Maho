7cc9fb2e27552ef0f5be8c0ed8eb28f30d9a7736
宏諭 柯 <tt960945@gmail.com>
2022-03-05 22:37:31 +0800

    BUG fix:[控刀手]幫報保留刀無須boss資訊

--- PCReDive_Maho/Event/SlashCommand/Controller/keep_knife.py
+++ PCReDive_Maho/Event/SlashCommand/Controller/keep_knife.py
@@ -10,19 +10,6 @@ import Module.Kernel.Update
                                   description="幫成員報一個保留刀",
                                   options=[
                                     create_option(
-                                      name="boss",
-                                      description="目標boss",
-                                      option_type=4,
-                                      required=True,
-                                      choices=[
-                                        create_choice(name="1王", value=1),
-                                        create_choice(name="2王", value=2),
-                                        create_choice(name="3王", value=3),
-                                        create_choice(name="4王", value=4),
-                                        create_choice(name="5王", value=5),
-                                      ]
-                                    ),
-                                    create_option(
                                       name="備註",
                                       description="備註（預計傷害）",
                                       option_type=3,
@@ -35,9 +22,9 @@ import Module.Kernel.Update
                                       required=True
                                     )
                                   ],
-                                  connector={"boss": "boss","備註": "comment","成員": "member"}
+                                  connector={"備註": "comment","成員": "member"}
                                 )
-async def keep_knife(ctx, boss, comment, member):
+async def keep_knife(ctx, comment, member):
   # check身分，並找出所屬組別
   connection = await Module.Kernel.DB_control.OpenConnection(ctx)
   if connection:
@@ -45,12 +32,12 @@ async def keep_knife(ctx, boss, comment, member):
     if not group_serial == 0: # 如果是是控刀手
       # 寫入保留刀表
       cursor = connection.cursor(prepared=True)
-      sql = "INSERT INTO princess_connect.keep_knifes (server_id, group_serial, member_id, boss, comment) VALUES (?, ?, ?, ?, ?)"
-      data = (ctx.guild.id, group_serial, member.id, boss, comment)
+      sql = "INSERT INTO princess_connect.keep_knifes (server_id, group_serial, member_id, comment) VALUES (?, ?, ?, ?)"
+      data = (ctx.guild.id, group_serial, member.id, comment)
       cursor.execute(sql, data)
       cursor.close()
       connection.commit()
-      await ctx.send(str(boss) + '王，備註:' + comment + '，**保留刀**報刀成功!')
+      await ctx.send('備註:' + comment + '，**保留刀**報刀成功!')
       await Module.Kernel.Update.Update(ctx, ctx.guild.id, group_serial) # 更新刀表
 
     await Module.Kernel.DB_control.CloseConnection(connection, ctx)
