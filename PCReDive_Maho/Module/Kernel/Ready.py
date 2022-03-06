bfb95b597b8849f0cca42acfca1626f97570eb45
宏諭 柯 <tt960945@gmail.com>
2022-03-06 14:15:19 +0800

    BUG fix:解決排程無法重複執行的問題

--- PCReDive_Maho/Module/Kernel/Ready.py
+++ PCReDive_Maho/Module/Kernel/Ready.py
@@ -1,7 +1,7 @@
 import discord
 import datetime
 import calendar
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import apscheduler.schedulers.asyncio 
 
 import Module.Kernel.Discord_client
 import Module.Kernel.auto_clear
@@ -10,28 +10,18 @@ import Module.Kernel.TopGG
 import Module.Kernel.Name_manager
 
 # 機器人上線事件
-@Module.Kernel.Discord_client.client.event
+@Module.Kernel.Discord_client.bot.event
 async def on_ready():
-  await Module.Kernel.Discord_client.client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="!h 查看更多資訊!"))
-  print('We have logged in as {0.user}'.format(Module.Kernel.Discord_client.client))
+  await Module.Kernel.Discord_client.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="輸入!h 查看更多資訊!"))
+  print('We have logged in as {0.user}'.format(Module.Kernel.Discord_client.bot))
 
   Module.Kernel.TopGG.update_stats.start() # 定期執行TOPGG，回傳狀態(30分鐘)
   Module.Kernel.Name_manager.clear_list.start() # 定期重置成員暱稱雜湊表(6小時)
-  task_manager() # 初次啟動及每月月初執行，取得開戰日期(當月倒數第5日)、清除日期(當月倒數第7日)、下次執行時間(次月第1日)
-
-def add_event_run_auto_clear(clear_date:datetime):
-  # 排定日期自動清理刀表，移除無用資訊(無法存取的伺服器)
-  sched = AsyncIOScheduler()  
-  sched.add_job(Module.Kernel.auto_clear.auto_clear, 'date', run_date = clear_date, args=[])
+  sched = apscheduler.schedulers.asyncio.AsyncIOScheduler(timezone='Asia/Taipei')  
+  task_manager(sched) # 初次啟動及每月月初執行，取得開戰日期(當月倒數第5日)、清除日期(當月倒數第7日)、下次執行時間(次月第1日)
   sched.start()
-
-def add_event_run_task_manager(clear_date:datetime):
-  # 排定日期重新設定開戰日期(當月倒數第5日)、清除日期(當月倒數第7日)、下次執行時間(次月第1日)
-  sched = AsyncIOScheduler()  
-  sched.add_job(task_manager, 'date', run_date = clear_date, args=[])
-  sched.start()
-
-def task_manager():
+  
+def task_manager(sched):
   # 當月開戰日期
   now_date = datetime.datetime.now()# 當前時間
   month_end_date = datetime.datetime(now_date.year, now_date.month, calendar.monthrange(now_date.year, now_date.month)[1])
@@ -48,15 +38,15 @@ def task_manager():
   datetime.datetime(year= Module.Kernel.define_value.this_year, month= Module.Kernel.define_value.this_month, day= Module.Kernel.define_value.this_day, hour=5, minute=0, second=0) + datetime.timedelta(days=4),\
   datetime.datetime(year= Module.Kernel.define_value.this_year, month= Module.Kernel.define_value.this_month, day= Module.Kernel.define_value.this_day, hour=0, minute=0, second=0) + datetime.timedelta(days=5)]
 
-  # 當月清除日期
+  # 取得當月清除日期，排定時間自動清理刀表以及移除無用資訊(無法存取的伺服器)
   clear_date = month_end_date - datetime.timedelta(days = 6) 
   if now_date < clear_date:
-    add_event_run_auto_clear(clear_date) # 排程清除任務
+    sched.add_job(Module.Kernel.auto_clear.auto_clear, 'date', run_date = clear_date, args=[]) # 排定日期
     print('當月重製時間為' + str(clear_date))
   else:
     print('當月重製時間已過')
 
-  # 下次執行日期
+  # 自身下次執行日期
   next_date = month_end_date + datetime.timedelta(days = 1)
+  sched.add_job(task_manager, 'date', run_date = next_date, args=[sched]) # 排定日期
   print('下次排程時間為' + str(next_date))
-  add_event_run_task_manager(next_date) # 排定自身下次執行時間 (月初1號)
