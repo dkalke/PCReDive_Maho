005b05dd9f758f41f3a769a5aa75742ab423468e
宏諭 柯 <tt960945@gmail.com>
2022-03-06 23:34:45 +0800

    BUG fix:必須優先載入參數

--- PCReDive_Maho/PCReDive_Maho.py
+++ PCReDive_Maho/PCReDive_Maho.py
@@ -1,10 +1,10 @@
 import os
 import dotenv
+dotenv.load_dotenv()
 
 import Module.Kernel.Discord_client
 import Module.Kernel.Ready
 import Event.Command
 import Event.SlashCommand.slash_command
 
-dotenv.load_dotenv()
 Module.Kernel.Discord_client.bot.run(os.getenv('TOKEN'))
