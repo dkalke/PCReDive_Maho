b99fa48afe1604b2524c3368cf43c3627f45c0da
宏諭 柯 <tt960945@gmail.com>
2023-03-30 15:52:19 +0800

    補償計算功能調整: - 移除理想補償傷害欄位(較無參考價值且避免誤會定義)

--- PCReDive_Maho/Module/General/calculate_damage.py
+++ PCReDive_Maho/Module/General/calculate_damage.py
@@ -14,7 +14,6 @@ async def calculate_damage(send_obj, remaining, damage1, damage2):
       retime = 90
     elif retime == 20:
       await send_obj.send('可能殺不死喔, 靠暴擊吧。')
-    redmg = round((damage2 / 90) * retime , 1)
 
     embed_msg = Embed(title='補償計算', color=0xD98B99)
     embed_msg.set_footer(text="出刀打王有賺有賠, 此資料僅供參考")
@@ -22,6 +21,5 @@ async def calculate_damage(send_obj, remaining, damage1, damage2):
     embed_msg.add_field(name="第一刀傷害", value=f"{damage1}萬", inline=True)
     embed_msg.add_field(name="第二刀傷害", value=f"{damage2}萬", inline=True)
     embed_msg.add_field(name="第二刀補償時間", value=f"{retime}秒", inline=True)
-    embed_msg.add_field(name="理想補償傷害", value=f"{redmg}萬", inline=True)
 
     await send_obj.send(embed=embed_msg)
